23a496eef23d370ac82c1ea768accc630ce824dc
thiago <desenvolvedor.nunes@gmail.com>
2023-05-26 09:59:46 -0300

    chore: adiciona assets e thema do app

--- src/theme/index.tsx
+++ src/theme/index.tsx
@@ -1,3 +1,36 @@
 export const theme = {
-  colors: {}
+  colors: {
+    red: {
+      50: '#F4E6E7',
+      450: '#F3BABD',
+      900: '#BF3B44'
+    },
+    green: {
+      50: '#E5F0DB',
+      450: '#CBE4B4',
+      900: '#639339'
+    },
+    gray: {
+      100: '#FFFFFF',
+      200: '#FAFAFA',
+      300: '#EFF0F0',
+      400: '#DDDEDF',
+      500: '#B9BBBC',
+      600: '#5C6265',
+      700: '#333638',
+      800: '#1B1D1E'
+    }
+  },
+  font_familly: {
+    regular: 'NunitoSans_400Regular',
+    bold: 'NunitoSans_700Bold'
+  },
+  font_size: {
+    sm: 12,
+    md: 14,
+    lg: 16,
+    xl: 18,
+    '2xl': 24,
+    '3xl': 32
+  }
 }
