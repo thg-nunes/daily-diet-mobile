7290ba0a5792331bcb9fee8f86ab7c6579cdbaf1
thiago <desenvolvedor.nunes@gmail.com>
2023-05-27 12:56:12 -0300

    feat: adiciona o scroll e titulo dos botoes

--- src/screens/create/index.tsx
+++ src/screens/create/index.tsx
@@ -9,9 +9,7 @@ import { OnDietButton } from '@components/onDietButton'
 
 import * as Styled from './styled'
 
-type CreateProps = {}
-
-export const Create = ({}: CreateProps) => {
+export const Create = () => {
   const [selectedDate, setSelectedDate] = useState('')
   const [selectedHour, setSelectedHour] = useState('')
   const [calendarIsOpen, setCalendarIsOpen] = useState(false)
@@ -35,79 +33,91 @@ export const Create = ({}: CreateProps) => {
           setSelectedHour={setSelectedHour}
         />
       )}
-
-      <Styled.Content>
-        <Input
-          inputLabel="Nome"
-          inputProps={{
-            maxLength: 50
-          }}
-        />
-        <Input
-          inputLabel="Descrição"
-          isMultiline
-          inputProps={{
-            multiline: true,
-            numberOfLines: 4,
-            textAlignVertical: 'top',
-            maxLength: 100
-          }}
-        />
-
-        <Styled.MealDateAndHourInfo>
-          <Styled.PressableContainer onPress={() => setCalendarIsOpen(true)}>
-            <Input
-              inputLabel="Data"
-              inputProps={{
-                value: selectedDate,
-                editable: false,
-                onChangeText: () => {
-                  setSelectedDate('')
-                  setCalendarIsOpen(true)
-                },
-                onFocus: () => setCalendarIsOpen(true)
-              }}
-            />
-          </Styled.PressableContainer>
-          <Styled.PressableContainer onPress={() => setClockIsOpen(true)}>
-            <Input
-              inputLabel="Hora"
-              inputProps={{
-                value: selectedHour,
-                editable: false,
-                onChangeText: () => {
-                  setSelectedHour('')
-                  setClockIsOpen(true)
-                },
-                onFocus: () => setClockIsOpen(true)
-              }}
-            />
-          </Styled.PressableContainer>
-        </Styled.MealDateAndHourInfo>
-
-        <Styled.MealChangeStateButton>
-          <OnDietButton
-            text="Sim"
-            buttonType="PRIMARY"
-            onDiet={mealOnDiet}
-            onPress={() => {
-              setMealOnDiet(true)
-              setMealOutDiet(undefined)
+      <Styled.Scroll showsVerticalScrollIndicator={false}>
+        <Styled.Content>
+          <Input
+            inputLabel="Nome"
+            inputProps={{
+              maxLength: 50
             }}
           />
-          <OnDietButton
-            text="Não"
-            buttonType="SECONDARY"
-            onDiet={mealOutDiet}
-            onPress={() => {
-              setMealOutDiet(true)
-              setMealOnDiet(undefined)
+          <Input
+            inputLabel="Descrição"
+            isMultiline
+            inputProps={{
+              multiline: true,
+              numberOfLines: 4,
+              textAlignVertical: 'top',
+              maxLength: 100,
+              style: {
+                height: 120,
+                maxHeight: 120
+              }
             }}
           />
-        </Styled.MealChangeStateButton>
 
-        <Button text="Cadastrar refeição" />
-      </Styled.Content>
+          <Styled.MealDateAndHourInfo>
+            <Styled.PressableContainer onPress={() => setCalendarIsOpen(true)}>
+              <Input
+                inputLabel="Data"
+                inputProps={{
+                  value: selectedDate,
+                  editable: false,
+                  onChangeText: () => {
+                    setSelectedDate('')
+                    setCalendarIsOpen(true)
+                  },
+                  onFocus: () => setCalendarIsOpen(true)
+                }}
+              />
+            </Styled.PressableContainer>
+            <Styled.PressableContainer onPress={() => setClockIsOpen(true)}>
+              <Input
+                inputLabel="Hora"
+                inputProps={{
+                  value: selectedHour,
+                  editable: false,
+                  onChangeText: () => {
+                    setSelectedHour('')
+                    setClockIsOpen(true)
+                  },
+                  onFocus: () => setClockIsOpen(true)
+                }}
+              />
+            </Styled.PressableContainer>
+          </Styled.MealDateAndHourInfo>
+
+          <Styled.OnDietButtonsSection>
+            <Styled.OnDietButtonsSectionHeader>
+              Está dentro da dieta?
+            </Styled.OnDietButtonsSectionHeader>
+            <Styled.MealChangeStateButton>
+              <OnDietButton
+                text="Sim"
+                buttonType="PRIMARY"
+                onDiet={mealOnDiet}
+                onPress={() => {
+                  setMealOnDiet(true)
+                  setMealOutDiet(undefined)
+                }}
+              />
+              <OnDietButton
+                text="Não"
+                buttonType="SECONDARY"
+                onDiet={mealOutDiet}
+                onPress={() => {
+                  setMealOutDiet(true)
+                  setMealOnDiet(undefined)
+                }}
+              />
+            </Styled.MealChangeStateButton>
+          </Styled.OnDietButtonsSection>
+
+          <Styled.ButtonContainer>
+            <Button text="Cadastrar refeição" />
+          </Styled.ButtonContainer>
+        </Styled.Content>
+      </Styled.Scroll>
     </Styled.Container>
   )
 }
