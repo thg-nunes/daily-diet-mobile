6d0e14c8907ce0c5884696fa5d20f2f68f176b97
thiago <desenvolvedor.nunes@gmail.com>
2023-05-30 15:58:13 -0300

    feat: cria navegacao para a screen de editar refeicao

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,8 +1,11 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 
+import { Meal } from '@utils/storage/meal/types'
+
 import { Home } from '@screens/home'
 import { Create } from '@screens/create'
 import { FeedBack } from '@screens/feedBack'
+import { EditeMeal } from '@screens/editeMeal'
 import { Statistics } from '@screens/statistics'
 import { MealsDetails, MealsDetailsProps } from '@screens/mealsDetails'
 import { UseMealsOfDietPercentResponse } from '@hooks/screens/home'
@@ -13,6 +16,7 @@ export type Routes = {
   FeedBack: {
     mealOnDiet: boolean
   }
+  EditeMeal: Meal
   MealsDetails: MealsDetailsProps
   Statistics: UseMealsOfDietPercentResponse
 }
@@ -29,6 +33,7 @@ export function AppRoutes(): JSX.Element {
       <Screen name="Home" component={Home} />
       <Screen name="New" component={Create} />
       <Screen name="FeedBack" component={FeedBack} />
+      <Screen name="EditeMeal" component={EditeMeal} />
       <Screen name="Statistics" component={Statistics} />
       <Screen name="MealsDetails" component={MealsDetails} />
     </Navigator>
