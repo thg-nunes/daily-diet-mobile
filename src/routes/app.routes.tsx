75e7c8b0bb9db557577ce74e03fb94206ae8add5
thiago <desenvolvedor.nunes@gmail.com>
2023-05-30 14:55:37 -0300

    feat: cria navegacao para a screen de feedback

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -2,12 +2,16 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack'
 
 import { Home } from '@screens/home'
 import { Create } from '@screens/create'
+import { FeedBack } from '@screens/feedBack'
 import { Statistics } from '@screens/statistics'
 import { UseMealsOfDietPercentResponse } from '@hooks/screens/home'
 
 export type Routes = {
   Home: undefined
   New: undefined
+  FeedBack: {
+    mealOnDiet: boolean
+  }
   Statistics: UseMealsOfDietPercentResponse
 }
 
@@ -22,6 +26,7 @@ export function AppRoutes(): JSX.Element {
     >
       <Screen name="Home" component={Home} />
       <Screen name="New" component={Create} />
+      <Screen name="FeedBack" component={FeedBack} />
       <Screen name="Statistics" component={Statistics} />
     </Navigator>
   )
