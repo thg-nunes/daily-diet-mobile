7e22a0344b90b233654e2680394ac844a72d1fb7
thiago nunes <desenvolvedor.nunes@gmail.com>
2023-06-02 14:56:38 -0300

    feat: adiciona scroll para quando o usuario for digitar nos inputs

--- src/screens/editeMeal/index.tsx
+++ src/screens/editeMeal/index.tsx
@@ -1,4 +1,4 @@
-import { View } from 'react-native'
+import { ScrollView, View } from 'react-native'
 
 import { useEditMeal } from '@hooks/screens/editeMeal'
 
@@ -10,9 +10,11 @@ import { GoBackHeader } from '@components/goBackHeader'
 import { OnDietButton } from '@components/onDietButton'
 
 import * as Styled from './styled'
+import { useTheme } from 'styled-components'
 
 export const EditeMeal = () => {
   const editeMeal = useEditMeal()
+  const { colors } = useTheme()
 
   return (
     <Styled.Container>
@@ -35,103 +37,105 @@ export const EditeMeal = () => {
         />
       )}
 
-      <Styled.Content>
-        <View
-          style={{
-            gap: 24,
-            flex: 1
-          }}
-        >
-          <Input
-            inputLabel="Nome"
-            inputProps={{
-              maxLength: 50,
-              onChangeText: editeMeal.setMealName,
-              value: editeMeal.mealName
-                ? editeMeal.mealName
-                : editeMeal.data.mealName
+      <ScrollView style={{ backgroundColor: colors.gray[100] }}>
+        <Styled.Content>
+          <View
+            style={{
+              gap: 24,
+              flex: 1
             }}
+          >
+            <Input
+              inputLabel="Nome"
+              inputProps={{
+                maxLength: 50,
+                onChangeText: editeMeal.setMealName,
+                value: editeMeal.mealName
+                  ? editeMeal.mealName
+                  : editeMeal.data.mealName
+              }}
+            />
+            <Input
+              inputLabel="Descrição"
+              isMultiline
+              inputProps={{
+                multiline: true,
+                numberOfLines: 4,
+                onChangeText: editeMeal.setMealDescription,
+                value: editeMeal.mealDescription
+                  ? editeMeal.mealDescription
+                  : editeMeal.data.description,
+                textAlignVertical: 'top',
+                maxLength: 100
+              }}
+            />
+
+            <Styled.MealDateAndHourInfo>
+              <Styled.PressableContainer
+                onPress={() => editeMeal.setCalendarIsOpen(true)}
+              >
+                <Input
+                  inputLabel="Data"
+                  inputProps={{
+                    value: editeMeal.selectedDate
+                      ? editeMeal.selectedDate
+                      : editeMeal.data.date,
+                    editable: false,
+                    onChangeText: () => editeMeal.setCalendarIsOpen(true),
+                    onFocus: () => editeMeal.setCalendarIsOpen(true)
+                  }}
+                />
+              </Styled.PressableContainer>
+              <Styled.PressableContainer
+                onPress={() => editeMeal.setClockIsOpen(true)}
+              >
+                <Input
+                  inputLabel="Hora"
+                  inputProps={{
+                    value: editeMeal.selectedHour
+                      ? editeMeal.selectedHour
+                      : editeMeal.data.hour,
+                    editable: false,
+                    onChangeText: () => editeMeal.setClockIsOpen(true),
+                    onFocus: () => editeMeal.setClockIsOpen(true)
+                  }}
+                />
+              </Styled.PressableContainer>
+            </Styled.MealDateAndHourInfo>
+
+            <Styled.OnDietButtonsSection>
+              <Styled.OnDietButtonsSectionHeader>
+                Está dentro da dieta?
+              </Styled.OnDietButtonsSectionHeader>
+              <Styled.MealChangeStateButton>
+                <OnDietButton
+                  text="Sim"
+                  buttonType="PRIMARY"
+                  onDiet={editeMeal.mealOnDiet}
+                  onPress={() => {
+                    editeMeal.setMealOnDiet(true)
+                    editeMeal.setMealOutDiet(undefined)
+                  }}
+                />
+                <OnDietButton
+                  text="Não"
+                  buttonType="SECONDARY"
+                  onDiet={editeMeal.mealOutDiet}
+                  onPress={() => {
+                    editeMeal.setMealOutDiet(true)
+                    editeMeal.setMealOnDiet(undefined)
+                  }}
+                />
+              </Styled.MealChangeStateButton>
+            </Styled.OnDietButtonsSection>
+          </View>
+
+          <Button
+            text="Salvar alterações"
+            onPress={editeMeal.handleUpdateMealData}
           />
-          <Input
-            inputLabel="Descrição"
-            isMultiline
-            inputProps={{
-              multiline: true,
-              numberOfLines: 4,
-              onChangeText: editeMeal.setMealDescription,
-              value: editeMeal.mealDescription
-                ? editeMeal.mealDescription
-                : editeMeal.data.description,
-              textAlignVertical: 'top',
-              maxLength: 100
-            }}
-          />
-
-          <Styled.MealDateAndHourInfo>
-            <Styled.PressableContainer
-              onPress={() => editeMeal.setCalendarIsOpen(true)}
-            >
-              <Input
-                inputLabel="Data"
-                inputProps={{
-                  value: editeMeal.selectedDate
-                    ? editeMeal.selectedDate
-                    : editeMeal.data.date,
-                  editable: false,
-                  onChangeText: () => editeMeal.setCalendarIsOpen(true),
-                  onFocus: () => editeMeal.setCalendarIsOpen(true)
-                }}
-              />
-            </Styled.PressableContainer>
-            <Styled.PressableContainer
-              onPress={() => editeMeal.setClockIsOpen(true)}
-            >
-              <Input
-                inputLabel="Hora"
-                inputProps={{
-                  value: editeMeal.selectedHour
-                    ? editeMeal.selectedHour
-                    : editeMeal.data.hour,
-                  editable: false,
-                  onChangeText: () => editeMeal.setClockIsOpen(true),
-                  onFocus: () => editeMeal.setClockIsOpen(true)
-                }}
-              />
-            </Styled.PressableContainer>
-          </Styled.MealDateAndHourInfo>
-
-          <Styled.OnDietButtonsSection>
-            <Styled.OnDietButtonsSectionHeader>
-              Está dentro da dieta?
-            </Styled.OnDietButtonsSectionHeader>
-            <Styled.MealChangeStateButton>
-              <OnDietButton
-                text="Sim"
-                buttonType="PRIMARY"
-                onDiet={editeMeal.mealOnDiet}
-                onPress={() => {
-                  editeMeal.setMealOnDiet(true)
-                  editeMeal.setMealOutDiet(undefined)
-                }}
-              />
-              <OnDietButton
-                text="Não"
-                buttonType="SECONDARY"
-                onDiet={editeMeal.mealOutDiet}
-                onPress={() => {
-                  editeMeal.setMealOutDiet(true)
-                  editeMeal.setMealOnDiet(undefined)
-                }}
-              />
-            </Styled.MealChangeStateButton>
-          </Styled.OnDietButtonsSection>
-        </View>
-
-        <Button
-          text="Salvar alterações"
-          onPress={editeMeal.handleUpdateMealData}
-        />
-      </Styled.Content>
+        </Styled.Content>
+      </ScrollView>
     </Styled.Container>
   )
 }
